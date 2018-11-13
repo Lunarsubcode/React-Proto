58cb16fb0bbe84e61829d3f09e21b870a4a0c3aa
Max McNeil <Max@Maxs-MacBook-Pro.local>
2018-11-13 10:43:13 -0500

    Text

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import Image from '../components/image'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>This is a React Prototype</h1>
+    <p>I'll figure this out</p>
+    <p>Now go build something great.</p>
+    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage
